e74dc1ec95c001f1d5cf6d62bbd499bd69ee6a88
Nova Solutions <fazal@novasolutions.dev>
2020-02-25 19:27:08 +0500

    aaaa

--- public/material/js/cs-script.js
+++ public/material/js/cs-script.js
@@ -6,10 +6,25 @@ $(document).ready(function() {
         console.log(slick_count);
         $(".custom-slider").slick({
             infinite: true,
-            // centerMode: true,
             slidesToShow: slick_count,
             slidesToScroll: 1,
-            // arrows: true
+            adaptiveHeight :true,
+            responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            }, {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true
+                }
+            }
+            ]
         });
     }
     else{
