84ee246f1c75da7368cc3d83286e8d90e2c176f9
Nova Solutions <fazal@novasolutions.dev>
2020-02-25 19:02:52 +0500

    aa

--- resources/views/admin/order-details.blade.php
+++ resources/views/admin/order-details.blade.php
@@ -136,15 +136,15 @@
                                                     @else
                                                         @if(count($categories) > 0)
                                                             @foreach($categories as $category)
-                                                                @if($category->name ==  $property->value)
+                                                                @if($category->name ==  $property['value'])
                                                                     <div class="pt-1 ml-2 " style="background:{{$category->color}}">
-                                                                        <h6 class="pr-2 pl-2 text-white pt-1"><b>{{ $property->value}}</b> </h6>
+                                                                        <h6 class="pr-2 pl-2 text-white pt-1"><b>{{ $property['value']}}</b> </h6>
                                                                     </div>
                                                                 @endif
                                                             @endforeach
                                                         @else
                                                             <div class="pt-1 ml-2 btn-blue">
-                                                                <h6 class="pr-2 pl-2 pt-1"><b>{{ $property->value}}</b> </h6>
+                                                                <h6 class="pr-2 pl-2 pt-1"><b>{{ $property['value']}}</b> </h6>
                                                             </div>
                                                         @endif
                                                     @endif
