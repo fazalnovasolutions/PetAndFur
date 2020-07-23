b514c4266a1e56512549b204383664a452603b8b
azanqureshi <azaan@tetralogicx.com>
2020-07-23 15:30:49 +0500

    azan

--- resources/views/layouts/customer.blade.php
+++ resources/views/layouts/customer.blade.php
@@ -73,7 +73,7 @@
 
                         <!-- Light Logo icon -->
                         <a href="{{route('customer.check')}}">
-                            <img src="{{asset('logo.png')}}" alt="homepage" height="80px" width="130px" class="light-logo" />
+                            <img src="{{asset('material/assets/images/logo.png')}}" alt="homepage" height="80px" width="130px" class="light-logo" />
                         </a>
                     </b>
                     <!--End Logo icon -->
