f396f278f233e1f4411cea8adc31ab14d41833dd
Ahmad Naeem <cp.chahmad@gmail.com>
2020-08-18 12:00:23 +0500

    'aa'

--- resources/views/customer/login.blade.php
+++ resources/views/customer/login.blade.php
@@ -43,7 +43,7 @@
 <section class="order-sec">
     <div class="container">
         <div class="row">
-            <div class="col-md-5 col-sm-5 col-xs-12 order-inner-f">
+            <div class="col-md-6 col-sm-6 col-xs-12 order-inner-f">
                 <div class="order-inner">
                     <h3>Login</h3>
 {{--                    <span class="order-num">Order Number</span>--}}
@@ -114,13 +114,10 @@
 {{--                            </div>--}}
 {{--                        </div>--}}
 
-                        <div class="col-md-7 col-sm-7 col-xs-12 p0">
+                        <div class="col-md-6 col-sm-6 col-xs-12 p0">
                             <div class="form-group">
                                     <div class="image-contain" style="
-                                    {{--background-image: url({{asset($b->image)}});--}}
-                                    {{--background-repeat: no-repeat;--}}
                                         background-size: cover;
-                                        max-width: 400px;
                                         margin: auto;
                                         background-position: center center;
                                         " >
